9c8edb90649748ab9a371a0b39b260495a9c5128
Win Brisbin <win@winbrisbin.com>
2013-12-07 14:59:11 -0600

    Web sites working - routes

--- spec/requests/inventory_enters_spec.rb
+++ spec/requests/inventory_enters_spec.rb
@@ -6,42 +6,41 @@ describe "InventoryEnters" do
 
   describe "Display page" do
 
-    it "should have the content 'WBR Inventory'" do
+    it "should have the content 'Display'" do
       visit '/inventory_enter/display'
-      expect(page).to have_content('WBR Inventory')
+      expect(page).to have_content('Display')
     end
 
 		it "should have the title 'Display'" do
       visit '/inventory_enter/display'
-      expect(page).to have_title("#{base_title} | Display")
+      expect(page).to have_title("WBR Inventory | Display")
     end
 
   end
 
   describe "Edit page" do
 
-    it "should have the content 'WBR Inventory'" do
+    it "should have the content 'Edit'" do
       visit '/inventory_enter/edit'
-      expect(page).to have_content('WBR Inventory')
+      expect(page).to have_content('Edit')
     end
 
 		it "should have the title 'Edit'" do
       visit '/inventory_enter/edit'
-      expect(page).to have_title("#{base_title} | Edit")
-    end
+       expect(page).to have_title("WBR Inventory | Edit")
 
   end
 
   describe "Lookup page" do
 
-    it "should have the content 'WBR Inventory'" do
+    it "should have the content 'Lookup'" do
       visit '/inventory_enter/lookup'
-      expect(page).to have_content('WBR Inventory')
-    end
+      expect(page).to have_content('Lookup')
+    end 
 
 		it "should have the title 'Lookup'" do
       visit '/inventory_enter/lookup'
-      expect(page).to have_title("#{base_title} | Lookup")
+      expect(page).to have_title("WBR Inventory | Lookup")
     end
 
   end
